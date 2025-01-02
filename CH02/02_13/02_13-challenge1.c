a9bcb0e163bfe270c85a3ae9c2fc5ac18cd20256
Aaron Bos <aaronmbos@gmail.com>
2025-01-02 12:34:12 +0000

    New fork

--- CH02/02_13/02_13-challenge1.c
+++ CH02/02_13/02_13-challenge1.c
@@ -3,19 +3,20 @@
 
 int main()
 {
-	//declare constant ratio equal to 3/4
-	//declare char a
-	//declare integer b
-	//declare float c
+	// Already did this in a deleted fork
+	// declare constant ratio equal to 3/4
+	// declare char a
+	// declare integer b
+	// declare float c
 
-	//assign char
-	//assign integer
-	//assign float
+	// assign char
+	// assign integer
+	// assign float
 
-	//output char value, e.g., "the value of variable a is '?'"
-	//output integer value, e.g., "the value of variable b is ??"
-	//output float value, e.g., "the value of variable c is ???.??"
-	//output value of ratio, e.g., "the value of constant ratio is ???.??"
+	// output char value, e.g., "the value of variable a is '?'"
+	// output integer value, e.g., "the value of variable b is ??"
+	// output float value, e.g., "the value of variable c is ???.??"
+	// output value of ratio, e.g., "the value of constant ratio is ???.??"
 
 	return 0;
 }
