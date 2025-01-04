648bc36c602880dfa083a3b417ff51502453fb8d
Aaron Bos <aaronmbos@gmail.com>
2025-01-04 12:23:58 +0000

    Complete chapter 4

--- CH04/04_09/04_09-challenge1.c
+++ CH04/04_09/04_09-challenge1.c
@@ -8,6 +8,19 @@ int main()
 	scanf("%d",&a);
 
 	/* write the loop here */
+	for (int i = 0; i < a; i++) 
+	{
+		putchar('-');
+	}
+
+	putchar('\n');
+
+	int i = 0;
+	while (i < a) 
+	{
+		putchar('*');
+		i++;
+	}
 
 	return(0);
 }
--- CH04/04_14/04_14-challenge1.c
+++ CH04/04_14/04_14-challenge1.c
@@ -7,6 +7,8 @@
 int main()
 {
 	// variable declarations
+	int done;
+	char in;
 			
 	/* main program loop */
 	done = FALSE;
@@ -14,12 +16,38 @@ int main()
 	{
 		// prompt for input
 		// process input
+		printf("Make a move:");
+		in = getchar();
+		switch (in)
+		{
+			case 'R':
+				puts("move right");
+				break;
+			case 'L':
+				puts("move left");
+				break;
+			case 'B':
+				puts("move back");
+				break;
+			case 'H':
+			case '?':
+				puts("Input R, L, B, or Q to quit");
+				break;
+			case 'Q':
+				done = TRUE;
+				break;
+			default:
+				puts("Invalid input");
+		}
 		// if R is input, output "move right"
 		// if L is input, output "move left"
 		// if B is input, output "move back"
 		// if Q is input, break the loop
 		// if H or ? is input, output valid commands
 		// identify invalid input
+		// Need to clear the buffer as the 
+		while((in=getchar()) != '\n')
+			;
 	}
 
 	return 0;
